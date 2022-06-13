7d96d7a501cadc422e783e7a5817335abd1547eb
jtorresr1 <jaimetr97@gmail.com>
2022-06-13 17:28:53 -0500

    Modified factory and seeder for Users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -10,15 +11,21 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
+    protected $model = User::class;
+
     public function definition()
     {
+        $minRangeAvailable = strtotime('1980-01-01');
+        $maxRangeAvailable = strtotime('2010-12-31');
         return [
             'name' => $this->faker->name(),
+            'birth_date' => $this->faker->dateTimeBetween($minRangeAvailable, $maxRangeAvailable),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
